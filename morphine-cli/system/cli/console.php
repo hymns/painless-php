1139305e98c3c443b814f1c11425eb0ea6f06453
Ruben <soggie@gmail.com>
2011-03-01 12:16:59 +0800

    Added beholder and a few other misc files

---
+++ morphine-cli/system/cli/console.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Painless PHP - the painless path to development
+ *
+ * Copyright (c) 2011, Tan Long Zheng (soggie)
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *  * Neither the name of Rendervault Solutions nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @package     Painless PHP
+ * @author      Tan Long Zheng (soggie) <ruben@rendervault.com>
+ * @copyright   2011 Tan Long Zheng (soggie) <ruben@rendervault.com>
+ * @license     BSD 3 Clause (New BSD)
+ * @link        http://painless-php.com
+ */
+
+class MorphineConsole extends PainlessConsole
+{
+    
+
+    protected function help( )
+    {
+        $output = <<<HELP
+Usage:
+  php morphine [generate|scaffold|update|install|test]
+
+Runtime options:
+  -f,[--force]      # Resolve conflicts by overwrting the old data (mutually exclusive with -s)
+  -s,[--skip]       # Resolve conflicts by skipping the new one data (mutually exclusive with -f)
+  -v,[--verbose]    # Prints out all status messages (mutually exclusive with -q)
+  -q,[--quiet]      # Surpresses all status messages (mutually exclusive with -v)
+  -h,[--help]       # Shows the help file
+
+HELP;
+        return $this->write( $output );
+    }
+}
---
+++ painless/library/redbean/redbean.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * Integration with RedbeanPHP 1.2 Legacy version (PHP 5.2)
+ */
+
+require_once dirname( __FILE__ ) . '/rb12lg.php';
