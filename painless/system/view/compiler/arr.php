afeb3e43e78fcd53e5c86475e91b02105fc31c80
Ruben <soggie@gmail.com>
2011-03-16 03:09:50 +0800

    Why do I keep forgetting to add files...

---
+++ painless/system/view/compiler/arr.php
@@ -0,0 +1,12 @@
+<?php
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Painless\System\View\Compiler;
+
+class Arr extends \Painless\System\View\Compiler\Base
+{
+
+}
