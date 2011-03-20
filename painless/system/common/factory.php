bda19068c059eab08861def449bee17b068f3931
Ruben <soggie@gmail.com>
2011-03-20 16:34:38 +0800

    Added email to factory

--- painless/system/common/factory.php
+++ painless/system/common/factory.php
@@ -7,6 +7,29 @@ namespace Painless\System\Common;
 
 class Factory
 {
+    public function email( $mailbox, $to, $subject, $template, $data = NULL, $type = \Painless\System\Common\Email::TEXT )
+    {
+        // Send a notification e-mail to the user
+        $response = \Painless::execute( \Painless::RUN_INTERNAL, 'POST email/index.html', array(
+            'mailbox'   => $mailbox,
+            'to'        => $to,
+            'subject'   => $subject,
+            'template'  => $template,
+            'data'      => $data,
+        ) );
+
+        // Only send the e-mail if the response code is 200
+        if ( $response->status === 200 )
+        {
+            $email = \Painless::load( 'system/common/email' );
+            $email->contentType( $type );
+            $email->content( $response->payload );
+            return $email;
+        }
+        
+        return NULL;
+    }
+    
     public function request( $method, $module, $controller, $params = array( ), $contentType = '', $agent = '' )
     {
         $request = \Painless::load( 'system/workflow/request', \Painless::LP_LOAD_NEW );
